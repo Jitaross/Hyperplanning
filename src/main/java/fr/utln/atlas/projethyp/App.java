68d804e5b16836948701d665dbf1a0d4be0ca124
Thomas Gouttebel <jitaross.clash@gmail.com>
2023-11-10 12:13:37 +0100

    first commit, init maven project

---
+++ src/main/java/fr/utln/atlas/projethyp/App.java
@@ -0,0 +1,13 @@
+package fr.utln.atlas.projethyp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
