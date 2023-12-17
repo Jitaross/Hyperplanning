336093643f1b4f6c4bbc2755379f2759667f6b4f
RICHARD Sébastien <sebastien-richard3@etud.univ-tln.fr>
2023-12-17 20:22:49 +0100

    improves absences and add the possibility to send a receipt

--- src/main/java/fr/utln/atlas/projethyp/controller/AbsencesController.java
+++ src/main/java/fr/utln/atlas/projethyp/controller/AbsencesController.java
@@ -3,6 +3,9 @@ package fr.utln.atlas.projethyp.controller;
 import javafx.fxml.FXML;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class AbsencesController {
     @FXML
@@ -12,6 +15,31 @@ public class AbsencesController {
     @FXML
     private void initialize(){
         this.borderPane.setVisible(false);
+
+    }
+
+    @FXML
+    private void handleFileChooser() {
+        FileChooser fileChooser = new FileChooser();
+
+        // Configurer le FileChooser (optionnel)
+        fileChooser.setTitle("Choisir un justificatif");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Tous les Fichiers", "*.*"),
+                new FileChooser.ExtensionFilter("Fichiers PDF", "*.pdf")
+                // Ajouter d'autres filtres si nécessaire
+        );
+
+        // Afficher la boîte de dialogue de sélection du justificatif
+        File selectedFile = fileChooser.showOpenDialog(null);
+
+        if (selectedFile != null) {
+            // Traiter le fichier sélectionné
+            System.out.println("Fichier sélectionné: " + selectedFile.getAbsolutePath());
+        } else {
+            // Aucun fichier n'a été sélectionné
+            System.out.println("Sélection de fichier annulée.");
+        }
     }
 
     public void show(){
