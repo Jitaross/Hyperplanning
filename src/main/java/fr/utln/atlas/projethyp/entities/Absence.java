220c26b4d07c551c5c188ebb7bf1745d079a0baf
Maanlex <alexandre@moquet.com>
2023-12-18 14:20:19 +0100

    Add AbsenceDAO and Absence

---
+++ src/main/java/fr/utln/atlas/projethyp/entities/Absence.java
@@ -0,0 +1,12 @@
+package fr.utln.atlas.projethyp.entities;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Absence implements Entity{
+	private int id;
+	private int idCours;
+	private int idEtudiant;
+}
